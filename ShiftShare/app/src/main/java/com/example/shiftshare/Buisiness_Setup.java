c97269b63ed31c784585f980f6af8642c7d8bb98
thedamncat <mlestes1@crimson.ua.edu>
2019-02-16 16:57:38 -0600

    added screen for business setup.

---
+++ ShiftShare/app/src/main/java/com/example/shiftshare/Buisiness_Setup.java
@@ -0,0 +1,13 @@
+package com.example.shiftshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Buisiness_Setup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buisiness__setup);
+    }
+}
