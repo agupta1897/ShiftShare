a7635fca26721755d38f688e3c1cdad7d8a6e1da
Brennan Jones <dabrenn12@gmail.com>
2019-02-21 13:48:52 -0600

    Manager Portal

---
+++ ShiftShare/app/src/main/java/com/example/firebasesetup/MPortal.java
@@ -0,0 +1,25 @@
+package com.example.firebasesetup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MPortal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mportal);
+
+        final TextView m_name = findViewById(R.id.nameEnter);
+        final TextView m_email = findViewById(R.id.emailEnter);
+        final TextView m_password = findViewById(R.id.passwordEnter);
+        final TextView m_phone = findViewById(R.id.phoneEnter);
+        final Button managerEnter = findViewById(R.id.managEnter);
+
+    }
+}
