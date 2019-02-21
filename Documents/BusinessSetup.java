e26436cb7a2df6e3c32fef98245039a65559a4ad
Murray Estes <mlestes1@crimson.ua.edu>
2019-02-20 21:23:08 -0600

    Modified Business class and BusinessSetup class

--- Documents/BusinessSetup.java
+++ Documents/BusinessSetup.java
@@ -2,6 +2,7 @@ package com.example.shiftshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,35 +11,51 @@ import business;
 
 public class Buisiness_Setup extends AppCompatActivity {
 
+    TextView input_name = findViewById(R.id.txt_input_bsn_name);
+    TextView input_num = findViewById(R.id.input_str_num);
+    TextView input_street = findViewById(R.id.input_bsn_street);
+    TextView input_city = findViewById(R.id.input_bsn_city);
+    TextView input_state = findViewById(R.id.input_bsn_state);
+    TextView input_zip = findViewById(R.id.input_bsn_zip);
+    Button btn_submit = findViewById(R.id.btn_submit);
+
+    DatabaseReference databaseManagers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buisiness__setup);
 
-        final TextView input_name = findViewById(R.id.txt_input_bsn_name);
-        final TextView input_num = findViewById(R.id.input_str_num);
-        final TextView input_street = findViewById(R.id.input_bsn_street);
-        final TextView input_city = findViewById(R.id.input_bsn_city);
-        final TextView input_state = findViewById(R.id.input_bsn_state);
-        final TextView input_zip = findViewById(R.id.input_bsn_zip);
-        final Button btn_submit = findViewById(R.id.btn_submit);
+        databaseManagers = FirebaseDatabase.getInstance().getReference("business");
 
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String bsn_name = input_name.getText().toString();
-                String bsn_store_num = input_num.getText().toString();
-                String bsn_street = input_street.getText().toString();
-                String bsn_city = input_city.getText().toString();
-                String bsn_state = input_state.getText().toString();
-                String bsn_zip = input_zip.getText().toString();
-                String bsn_id = databaseManagers.push().getKey();
-                Business bsn = new Business(bsn_id, bsn_name, bsn_store_num, bsn_street, bsn_city, bsn_state, bsn_zip);
-                databaseManagers.child(id).setValue(bsn);
-                Toast.makeText(v.getContext(), "Data Submitted", Toast.LENGTH_SHORT).show();
-
+                
             }
         });
 
+    }
+
+    private void add_business(){
+        String bsn_name = input_name.getText().toString();
+        String bsn_store_num = input_num.getText().toString();
+        String bsn_street = input_street.getText().toString();
+        String bsn_city = input_city.getText().toString();
+        String bsn_state = input_state.getText().toString();
+        String bsn_zip = input_zip.getText().toString();
+        
+        if(TextUtils.isEmpty(bsn_name) || 
+           TextUtils.isEmpty(bsn_street) || 
+           TextUtils.isEmpty(bsn_city) || 
+           TextUtils.isEmpty(bsn_state) ||
+           TextUtils.isEmpty(bsn_zip))
+        {Toast.makeText(this, "There are required fields empty", Toast.LENGTH_SHORT).show();}
+        else{
+            String id = databaseManagers.push().getKey();
+            Business bsn = new Business(bsn_id, bsn_name, bsn_store_num, bsn_street, bsn_city, bsn_state, bsn_zip);
+            databaseManagers.child(id).setValue(bsn);
+            Toast.makeText(v.getContext(), "Data Submitted", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- Documents/business.java
+++ Documents/business.java
@@ -1,3 +1,5 @@
+package com.example.firebasesetup;
+
 public class Business{
 
     private String id;
