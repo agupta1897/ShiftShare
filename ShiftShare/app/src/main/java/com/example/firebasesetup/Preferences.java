32f934fa056c77dd2ce204f77bc6da7e84e2cb39
thedamncat <mlestes1@crimson.ua.edu>
2019-03-30 14:26:00 -0500

    Added methods to store login status and user ID. (currently untested)

---
+++ ShiftShare/app/src/main/java/com/example/firebasesetup/Preferences.java
@@ -0,0 +1,34 @@
+package com.example.firebasesetup;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+
+public class Preferences {
+
+    public static SharedPreferences getPreferences(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static void setLogin(Context context, boolean login){
+        SharedPreferences.Editor editor = getPreferences(context).edit();
+        editor.putBoolean("LOGIN_PREF", login);
+        editor.apply();
+    }
+
+    public static boolean getLogin(Context context){
+        return getPreferences(context).getBoolean("LOGIN_PREF", false);
+    }
+
+    public static void setId(Context context, String id){
+        SharedPreferences.Editor editor = getPreferences(context).edit();
+        editor.putString("USER_ID", id);
+        editor.apply();
+    }
+
+    public static String getId(Context context){
+        return getPreferences(context).getString("USER_ID", null);
+    }
+
+}
